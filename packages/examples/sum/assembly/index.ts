73bee16fb1d341fa68530bd356d9bafbc9684b48
Dmitry Osipov <dmith.osipov@gmail.com>
2023-05-09 10:32:07 +0400

    store / load vars

--- packages/examples/sum/assembly/index.ts
+++ packages/examples/sum/assembly/index.ts
@@ -18,24 +18,41 @@ class State {
   }
 }
 
-let STATE = new State();
+let STATE = new Array<State>(1);
+let MESSAGE_LOG = new Array<Vec<ScaleString>>(1);
 
-let MESSAGE_LOG = new Vec<ScaleString>();
+const globStatePtr = STATE.dataStart;
+const globMessageLogPtr = MESSAGE_LOG.dataStart;
+
+store<Array<State>>(globStatePtr, STATE);
+store<Array<Vec<ScaleString>>>(globMessageLogPtr, MESSAGE_LOG);
 
 export function init(): void {
   let bytes = new Uint8Array(size());
   read(bytes);
 
+  debug(`(init) globStatePtr: ${globStatePtr}`);
+  debug(`(init) globMessageLogPtr: ${globMessageLogPtr}`);
+
+  STATE[0] = new State();
+  MESSAGE_LOG[0] = new Vec<ScaleString>();
+
   const actor = new ActorId(bytes);
 
-  STATE.sendTo = Option.Some<ActorId>(actor);
-  debug(`(init) sentTo.isSome: ${STATE.sendTo.isSome}`);
+  STATE[0].sendTo = Option.Some<ActorId>(actor);
+  debug(`(init) sentTo.isSome: ${STATE[0].sendTo.isSome}`);
 }
 
 export function handle(): void {
-  debug(`(handle) start...`);
+  debug(`(handle) globStatePtr: ${globStatePtr}`);
+  debug(`(handle) globMessageLogPtr: ${globMessageLogPtr}`);
+  STATE = load<Array<State>>(globStatePtr);
+  MESSAGE_LOG = load<Array<Vec<ScaleString>>>(globMessageLogPtr);
+
   const bytes = new Uint8Array(size());
   read(bytes);
+
+  debug(`(handle) sentTo.isSome: ${STATE[0].sendTo.isSome}`);
 
   debug(`(handle) bytes: ${bytes}`);
 
@@ -43,26 +60,24 @@ export function handle(): void {
 
   debug(`(handle) new_msg: ${new_msg}`);
 
-  MESSAGE_LOG.push(ScaleString.from(`(sum) New msg: ${new_msg}`));
+  MESSAGE_LOG[0].push(ScaleString.from(`(sum) New msg: ${new_msg}`));
 
-  debug(`(handle) messages length: ${MESSAGE_LOG.length}`);
+  debug(`(handle) messages length: ${MESSAGE_LOG[0].length}`);
 
   const sum = new_msg.add(new_msg);
 
-  new Uint8Array(16).fill(0);
   debug(`(handle) sum: ${sum}`);
 
-  debug(`(handle) sentTo.isSome: ${STATE.sendTo.isSome}`);
-
-  if (STATE.sendTo.isSome) {
+  if (STATE[0].sendTo.isSome) {
     debug(`(handle) send msg`);
-    send(STATE.sendTo.unwrap(), sum.encode(), u128.Zero);
+    send(STATE[0].sendTo.unwrap(), sum.encode(), u128.Zero);
     debug('(handle) msg sent');
   }
 
-  debug(`(handle) ${MESSAGE_LOG.length} total message stored:`);
-
-  for (let i = 0; i < MESSAGE_LOG.length; i++) {
-    debug(`(handle) ${MESSAGE_LOG[i]}`);
-  }
+  debug(`(handle) total message stored: ${MESSAGE_LOG[0].length} `);
+  store<Array<State>>(globStatePtr, STATE);
+  store<Array<Vec<ScaleString>>>(globMessageLogPtr, MESSAGE_LOG);
+  // for (let i = 0; i < MESSAGE_LOG[0].length; i++) {
+  //   debug(`(handle) ${MESSAGE_LOG[0][i]}`);
+  // }
 }
